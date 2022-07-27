ba0030ef22b98bacbcde7d431515fc90159fabbe
ansistolar <1274355439@qq.com>
2022-07-27 12:31:28 +0800

    提交初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h5>头条</h5>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less"></style>
